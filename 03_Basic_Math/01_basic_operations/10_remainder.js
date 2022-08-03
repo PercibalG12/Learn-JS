ca0c6e38bbb5cd14d6bc2c2aca45a6aabfaaef53
PercibalG12 <percipromotionsyndicate.com>
2022-08-03 12:08:27 -0500

    minor type error fixes

--- 03_Basic_Math/01_basic_operations/10_remainder.js
+++ 03_Basic_Math/01_basic_operations/10_remainder.js
@@ -1,10 +1,10 @@
 /*  the ramainder operator (%) gets the remainder of the division of two numbers. 
-It is often times reffred to as the modulus operator but the two are actually different */
+ */
 
 var remainder;
-remainder = 11 % 3;
+remainder = 11 % 5;
 
-// In this case  11 /3 = 9 and 11-9  is equal to 2 and so the remainder is 2 
+
 
 
 // use cases
